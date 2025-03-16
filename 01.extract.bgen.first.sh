041128b496139982ab758dbef0ceb31407dff6e8
suiyangsun <37144891+suiyangsun@users.noreply.github.com>
2025-03-15 20:24:40 -0400

    Add files via upload

---
+++ 01.extract.bgen.first.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+##########part1:extract the region for calculate PRS
+####this is design for UKB, because the bgen file is 'ref-first'
+
+#####author:Yang Sui(ysui@broadinsitute.org)
+#####Date:2025.03.15
+
+# Print the total number of arguments
+echo "Total number of arguments: $#"
+
+# Accessing each argument by its position
+echo "Plink Argument: $1"
+echo "bgen Argument: $2"
+echo "sample Argument: $3"
+echo "extract Argument: $4"
+echo "out Argument: $5"
+
+
+$1 \
+--bgen $2 'ref-first' \
+--sample $3 \
+--extract bed0 $4 \
+--make-pgen \
+--out $5
+
+
+
